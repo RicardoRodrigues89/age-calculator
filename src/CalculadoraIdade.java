9f4d22e67f7902b1b220fdb1531c43c14e5aa836
Ricardo Rodrigues <ricardo.rodrigues@alertbrasil.com.br>
2026-04-06 11:56:28 -0300

    Adiciona calculadora de idade por ano de nascimento

---
+++ src/CalculadoraIdade.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculadoraIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o ano atual: ");
+        int anoAtual = scanner.nextInt();
+
+        System.out.println("Digite seu ano de nascimento: ");
+        int anoNascimento = scanner.nextInt();
+
+        int idade = anoAtual - anoNascimento;
+
+        System.out.println(nome + ", você tem " + idade + " anos de idade.");
+
+        scanner.close();
+    }
+}
